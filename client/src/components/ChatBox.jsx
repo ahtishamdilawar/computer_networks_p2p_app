e833425630978fc2e3d4d041af9e625840856d38
ahtishamdilawar <126797321+ahtishamdilawar@users.noreply.github.com>
2024-12-09 19:14:24 +0500

    shayaned all over them

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -1,10 +1,16 @@
-
+import "./App.css";
+import "./src/index.css";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import { Input } from "@/components/ui/input";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Send, Upload, File, Check, X, Download } from "lucide-react";
+const CustomButton = (props) => (
+  <Button {...props} className={`bg-red-500 hover:bg-red-600 text-white ${props.className || ""}`}>
+    {props.children}
+  </Button>
+);
 const ChatBox = ({
     peerId,
     connections,
@@ -14,8 +20,8 @@ const ChatBox = ({
     downloadFile,
   }) => (
     <div className="peer-window border-2 rounded-lg p-4">
-      <h2 className="text-xl font-bold mb-3">
-        Chat with <Badge variant="secondary">{peerId}</Badge>
+      <h2 className="text-xl font-bold ml-3">
+        Chat with <Badge variant="secondary"><span style={{ fontSize: '20px' }}>{peerId}</span></Badge>
       </h2>
       <ScrollArea style={{ maxHeight: "400px" }}>
         {connections.get(peerId).map((msg, idx) => (
@@ -28,4 +34,6 @@ const ChatBox = ({
       <MessageInput peerId={peerId} sendMessage={sendMessage} handleFileChange={handleFileChange} />
     </div>
   );
+ 
+
   
